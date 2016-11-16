b9e927219ca77ed82c4c62f395b4b2cb2ba6b338
Mathias Düsterhöft <mduesterhoeft@epages.com>
2016-11-16 22:01:39 +0100

    extract OrderProtocols

--- src/main/scala/com/example/OrderActor.scala
+++ src/main/scala/com/example/OrderActor.scala
@@ -69,41 +69,6 @@ object OrderActor {
   case class OrderCreated(order: Order) extends OrderEvent
   case class OrderStatusSet(orderStatus: OrderStatus, comment: String) extends OrderEvent
 
-  object OrderProtocols extends SprayJsonSupport with DefaultJsonProtocol {
-    implicit object OrderStatusFormat extends RootJsonFormat[OrderStatus] {
-      //see https://github.com/spray/spray-json/issues/186
-      private val mapping = Seq(Open, Complete, Cancelled).map(obj ⇒ key(obj) -> obj).toMap
-
-      def write(c: OrderStatus) = JsString(key(c))
-
-      def read(value: JsValue): OrderStatus = (value match {
-        case JsString(value) => mapping.get(value)
-        case _ => None
-      }).getOrElse(throw new MatchError(s"not a valid OrderStatus $value"))
-
-      def key(obj: OrderStatus): String = {
-        obj.toString.toLowerCase
-      }
-    }
-    implicit val priceFormat = jsonFormat2(Price)
-    implicit val productFormat = jsonFormat3(Product)
-    implicit val orderFormat = orderStatusDefaultJsonFormat(jsonFormat4(Order))
-
-    private def orderStatusDefaultJsonFormat[T](format: RootJsonFormat[T]): RootJsonFormat[T] = new RootJsonFormat[T] {
-      override def write(obj: T): JsValue = {
-        format.write(obj).asJsObject
-      }
-
-      override def read(json: JsValue): T = {
-        val order = json.asJsObject
-        format.read(order.fields.contains("orderStatus") match {
-          case true => order
-          case false => order.copy(fields = order.fields.updated("orderStatus", OrderStatusFormat.write(Open)))
-        })
-      }
-    }
-  }
-
   trait OrderFlow  {
     import OrderProtocols._
 
